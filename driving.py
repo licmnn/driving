cfaa08a5282d6b663de20604dfcb5cb47481ce36
licmnn <licmn@live.com>
2020-05-19 21:33:07 +0800

    add Us part

--- driving.py
+++ driving.py
@@ -3,7 +3,11 @@ age = input('请输入您的年龄： ')
 age = int(age)
 if country == '台湾':
 	if age >= 18:
-
 		print('你可以考驾照')
 	else:
 		print('你还不能考驾照')
+elif country == '美国':
+	if age >= 16:
+		print('你可以考驾照')
+	else:
+		print('你不可以考驾照')
